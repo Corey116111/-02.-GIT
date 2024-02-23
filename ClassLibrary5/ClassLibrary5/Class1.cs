825f7861712d8ebb00ad5392ff048a1c0467f6c4
vika-1234 <150289555+vika-1234@users.noreply.github.com>
2024-02-23 23:54:49 +0300

    Add files via upload

---
+++ ClassLibrary5/ClassLibrary5/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary5
+{
+
+    public class Discipline
+    {
+        private string name; private string shortName;
+        public Discipline(string name, string shortName)
+        {
+            this.name = name; this.shortName = shortName;
+        }
+    }
+
+}
---
+++ ClassLibrary5/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary5;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Стыднаааа!!!");
+        }
+    }
+}
