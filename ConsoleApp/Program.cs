a4c9672c0dac662044fe8513a36fa43088628aa5
SelfKnowledge <147917401+Se1fKnowledge@users.noreply.github.com>
2024-02-24 01:42:57 +0300

    Add files via upload

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using OrganisationLibrary;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("It's working, Johnny");
+    }
+}
---
+++ Organisation/Organisation.cs
@@ -0,0 +1,18 @@
+﻿// https://docs.google.com/document/d/1JN0SjEeelhtpKzoJqkva5JRePiMCLzyYPcI0Je8m7qU/edit
+namespace OrganisationLibrary
+{
+    public class Organisation
+    {
+        private string name;
+        private string legalAdress;
+        private string actualAdress;
+        private string refSupervisor;
+        public Organisation(string name, string legalAdress, string actualAdress, string refSupervisor)
+        {
+            this.name = name;
+            this.legalAdress = legalAdress;
+            this.actualAdress = actualAdress;
+            this.refSupervisor = refSupervisor;
+        }
+    }
+}
